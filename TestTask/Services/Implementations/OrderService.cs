a4c45f93d65e5ce0d9c2c73d271b810db0af47b6
KrotovAV <KrotovAV@tut.by>
2024-05-31 18:00:13 +0300

    done

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.ConstrainedExecution;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // User { int Id, string Email, UserStatus Status,  List<Order> Orders}
+        // Order { int Id, string ProductName, int Price, int Quantity, int UserId, DateTime CreatedAt, OrderStatus Status, User User}
+
+        public async Task<Order> GetOrder() 
+        {
+            //3.Возвращать самый новый заказ, в котором больше одного предмета. 
+            var order = await _context.Orders
+                .Where(x => (x.Quantity > 1))
+                .OrderByDescending(x => x.CreatedAt)
+                .FirstOrDefaultAsync();
+
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders() 
+        {
+            //4.Возвращать заказы от активных пользователей, отсортированные по дате создания.
+            var orders = await _context.Orders
+                .Where(x => (x.User.Status == UserStatus.Active))
+                .OrderByDescending(x => x.CreatedAt)
+                .ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // User { int Id, string Email, UserStatus Status,  List<Order> Orders}
+        // Order { int Id, string ProductName, int Price, int Quantity, int UserId, DateTime CreatedAt, OrderStatus Status, User User}
+
+        public async Task<User> GetUser() 
+        {
+            //1.Возвращать пользователя с максимальной общей суммой товаров, доставленных в 2003
+            var user = await _context.Orders
+                .Where(x => (x.CreatedAt.Year == 2003) && (x.Status == OrderStatus.Delivered))
+                .GroupBy(o => o.User)
+                .Select(x => new {user = x.Key, maxCoast = x.Sum(o => o.Price * o.Quantity)})
+                .OrderByDescending(x => x.maxCoast)
+                .Select(x => x.user)
+                .FirstOrDefaultAsync();
+
+            return user;
+        }
+
+        public async Task<List<User>> GetUsers() 
+        {
+            //2.Возвращать пользователей у которых есть оплаченные заказы в 2010
+            var users = await _context.Orders
+              .Where(x => (x.CreatedAt.Year == 2010) && (x.Status == OrderStatus.Paid))
+              .GroupBy(o => o.User)
+              .Select(x => x.Key)
+              .ToListAsync();
+
+            return users;
+        }
+    }
+}
